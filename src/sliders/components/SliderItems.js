121c0495dadd5372cf363aed299d2389acbc6c7a
zhufengnodejs <zhufengnodejs@126.com>
2017-11-30 17:15:04 +0800

    init

---
+++ src/sliders/components/SliderItems.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+export default class SliderItems extends Component {
+  render() {
+    return (
+      <ul>
+
+      </ul>
+    )
+  }
+}
---
+++ src/sliders/components/Sliders.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './Sliders.css'
+export default class Sliders extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        Sliders
+      </div>
+    )
+  }
+}
