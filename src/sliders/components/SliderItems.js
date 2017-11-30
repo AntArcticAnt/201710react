1618db8ca22bc9ff8afb502274e7a3c04932b5b3
zhufengnodejs <zhufengnodejs@126.com>
2017-11-30 17:43:28 +0800

    init

--- src/sliders/components/SliderItems.js
+++ src/sliders/components/SliderItems.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 export default class SliderItems extends Component {
   render() {
+    let style = {
+      width:this.props.images.length*300+'px',
+      left:this.props.index*-300+'px',
+      transitionDuration:this.props.speed+'s'
+    }
     return (
-      <ul className="sliders">
+      <ul className="sliders" style={style}>
         {
           this.props.images.map((item, index) => (
-            <li className="slider">
+            <li key={index} className="slider">
               <img src={item}/>
             </li>
           ))
--- src/sliders/components/Sliders.js
+++ src/sliders/components/Sliders.js
@@ -2,10 +2,32 @@ import React, {Component} from 'react';
 import './Sliders.css'
 import SliderItems from "./SliderItems";
 export default class Sliders extends Component {
+  constructor() {
+    super();
+    this.state = {index: 0};
+  }
+  turn = (step)=>{
+
+    this.setState({index: this.state.index + step});
+  }
+  go = () => {
+    this.timerId = setInterval(() => {
+      this.turn(1);
+    }, this.props.delay * 1000);
+  }
+
+  componentDidMount() {
+    if(this.props.autoPlay)
+      this.go();
+  }
+
   render() {
     return (
       <div className="wrapper">
-        <SliderItems images={this.props.images}/>
+        <SliderItems
+          index={this.state.index}
+          speed={this.props.speed}
+          images={this.props.images}/>
       </div>
     )
   }
