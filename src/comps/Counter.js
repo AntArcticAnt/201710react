70beb17d5740d999794ace1b98f761580b9c3abe
zhufengnodejs <zhufengnodejs@126.com>
2017-12-05 15:27:52 +0800

    init

--- src/comps/Counter.js
+++ src/comps/Counter.js
@@ -7,12 +7,26 @@ import store from '../store';
  * 3. 让当前组件订阅仓库状态变化事件，当仓库状态发生改变后要生渲染组件
  */
 export default class Counter extends Component {
+  constructor(){
+    super();
+    this.state = {number:store.getState().number};
+  }
+  //从 React16开始，不要再用WillMount,因为WillMount可能会触发多次
+  componentDidMount(){
+    this.unSubscribe = store.subscribe(()=>{
+      this.setState({number:store.getState().number});
+    })
+  }
+  componentWillUnmount(){
+    this.unSubscribe();
+  }
+
   render() {
     return (
       <div>
-        <p>{store.getState().number}</p>
+        <p>{this.state.number}</p>
         <button onClick={()=>store.dispatch({type:types.ADD})}>+</button>
-        <button>-</button>
+        <button onClick={()=>store.dispatch({type:types.SUB})}>-</button>
       </div>
     )
   }
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import {createStore} from '../redux';
+//是一个纯函数 1.输入一定输出一定 2 不改变参数
 import reducer from './reducer';
 let store = createStore(reducer);
 export default store;
