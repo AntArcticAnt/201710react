357bc93b17878700365c08cd4fa0cf74c130e84d
zhufengnodejs <zhufengnodejs@126.com>
2017-12-05 11:39:02 +0800

    init

--- app3/index2.js
+++ app3/index2.js
@@ -1,6 +1,6 @@
-
 //函数作用域可以保护变量
-function createStore(){
+//创建仓库的时候需要传进来一个处理器
+function createStore(reducer){
 //数据源，也就是初始状态或者叫初始数据
   // redux规定，仓库每收到action之后不能修改原始的状态对象，而是一定返回一个新的状态对象
   let state = {
@@ -19,27 +19,47 @@ function createStore(){
   //action是一个动作对象，描述了想发射的动作类型,必须有一个type属性
 //如果有人想改变状态，唯一的途径就是调用此方法
   //因为外部无法修改状态，所以只能放在函数里
+  //接收action,返回新状态对象
   function dispatch(action){
-    //判断动作类型
-    switch(action.type){
-      case 'UPDATE_TITLE_TEXT'://更新标题的内容
-        //状态具有不变性，永远不要直接修改老状态对象
-        state= {...state,title:{...state.title,text:action.text}};
-        break;
-      case 'UPDATE_CONTENT_COLOR'://更新内容的颜色
-        state= {...state,content:{...state.content,color:action.color}};
-        break;
-      default:
-        break;
+    //处理器接收老状态和动作，返回新状态 ，然后通知所有的监听函数执行
+    state = reducer(state,action);
+    //让保存起来的每个监听函数立刻执行
+    listeners.forEach(listener=>listener());
+  }
+  //订阅仓库中的状态变化事件,需要传入一个回调函数(监听函数),当仓库中状态发生变化的时候会调用这些监听函数
+  let listeners = [];
+  let subscribe = (listener)=>{
+    //监听函数先不执行，先保存在仓库的内部
+    listeners.push(listener);
+    //订阅函数会返回一个取消订阅的函数
+    return function(){
+      //循环监听数组，保留那些不等于当前监听函数的元素
+      listeners = listeners.filter(item=>item!=listener);
     }
   }
   return {
-    getState,
-    dispatch
+    getState,//获取当前的最新的状态
+    dispatch,//派发动作
+    subscribe
+  }
+}
+//state是老状态 上一个状态对象
+//action是动作对象
+//返回新的状态对象
+function reducer(state,action){
+  //判断动作类型
+  switch(action.type){
+    case 'UPDATE_TITLE_TEXT'://更新标题的内容
+      //状态具有不变性，永远不要直接修改老状态对象
+      return  {...state,title:{...state.title,text:action.text}};
+    case 'UPDATE_CONTENT_COLOR'://更新内容的颜色
+      return  {...state,content:{...state.content,color:action.color}};
+    default:
+      break;
   }
 }
 //调用createStore方法返回一个仓库对象
-let store = createStore();
+let store = createStore(reducer);
 //派发动作，如果有谁想改状态，必须调用方法
 
 function renderApp(){
@@ -57,12 +77,14 @@ function renderContent(){
   content.style.color = store.getState().content.color||store.getState().defaultColor;
 }
 renderApp();
+//当仓库中的状态变化之后执行renderApp方法
+store.subscribe(renderApp);
 setTimeout(function(){
-  dispatch({type:'UPDATE_TITLE_TEXT',text:'新标题'});
-  dispatch({type:'UPDATE_CONTENT_COLOR',color:'orange'});
-  dispatch({type:'DELETE',color:'orange'});
-  renderApp();
+  store.dispatch({type:'UPDATE_TITLE_TEXT',text:'新标题'});
 },3000);
+setTimeout(function(){
+  store.dispatch({type:'UPDATE_CONTENT_COLOR',color:'orange'});
+},6000);
 /**
  * 1.虽然我们规定只能通过dispatch来修改状态，但是我们还是无法阻止 直接修改状态 ，所以状态需要被保护起来，以防随意改变
  * 2. dispatch修改状态之后，还需要手工重新调用渲染方法，比较麻烦，所 以我们希望当状态改变之后自动重新渲染
