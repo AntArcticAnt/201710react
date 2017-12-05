85714ca68eca338b31cf02f6286da41d1137ea1a
zhufengnodejs <zhufengnodejs@126.com>
2017-12-05 11:02:56 +0800

    init

--- app3/index2.js
+++ app3/index2.js
@@ -2,6 +2,7 @@
 //函数作用域可以保护变量
 function createStore(){
 //数据源，也就是初始状态或者叫初始数据
+  // redux规定，仓库每收到action之后不能修改原始的状态对象，而是一定返回一个新的状态对象
   let state = {
     defaultColor:'blue',//默认颜色
     title:{
@@ -13,30 +14,34 @@ function createStore(){
       color:'green'
     }
   }
-  //定义一个获取状态的方法
-  let getState = ()=>state;
+  //定义一个获取状态的方法,为了防止外部修改，一般把状态对象进行深度克隆
+  let getState = ()=>JSON.parse(JSON.stringify(state));
+  //action是一个动作对象，描述了想发射的动作类型,必须有一个type属性
+//如果有人想改变状态，唯一的途径就是调用此方法
+  //因为外部无法修改状态，所以只能放在函数里
+  function dispatch(action){
+    //判断动作类型
+    switch(action.type){
+      case 'UPDATE_TITLE_TEXT'://更新标题的内容
+        //状态具有不变性，永远不要直接修改老状态对象
+        state= {...state,title:{...state.title,text:action.text}};
+        break;
+      case 'UPDATE_CONTENT_COLOR'://更新内容的颜色
+        state= {...state,content:{...state.content,color:action.color}};
+        break;
+      default:
+        break;
+    }
+  }
   return {
-    getState
+    getState,
+    dispatch
   }
 }
 //调用createStore方法返回一个仓库对象
 let store = createStore();
 //派发动作，如果有谁想改状态，必须调用方法
-//action是一个动作对象，描述了想发射的动作类型,必须有一个type属性
-//如果有人想改变状态，唯一的途径就是调用此方法
-function dispatch(action){
-  //判断动作类型
-  switch(action.type){
-    case 'UPDATE_TITLE_TEXT'://更新标题的内容
-      state.title.text = action.text;//还要传过来新的标题
-      break;
-    case 'UPDATE_CONTENT_COLOR'://更新内容的颜色
-     state.content.color = action.color;
-     break;
-    default:
-      break;
-  }
-}
+
 function renderApp(){
   renderTitle();
   renderContent();
