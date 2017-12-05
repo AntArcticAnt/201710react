42c80f381620441d51ee9e267aedbc682cdba260
zhufengnodejs <zhufengnodejs@126.com>
2017-12-05 13:03:35 +0800

    init

--- src/comps/Counter.js
+++ src/comps/Counter.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import * as types from '../store/action-types';
 import store from '../store';
+/**
+ * 1. 完成reducer
+ * 2. 绑定减1的事件处理函数
+ * 3. 让当前组件订阅仓库状态变化事件，当仓库状态发生改变后要生渲染组件
+ */
 export default class Counter extends Component {
   render() {
     return (
