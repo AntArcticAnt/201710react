ec6de62709b39a9bbca68d01bca8d696f0d75dbc
zhufengnodejs <zhufengnodejs@126.com>
2017-12-10 17:55:55 +0800

    init

--- src/sliders/components/SliderDots.js
+++ src/sliders/components/SliderDots.js
@@ -8,7 +8,7 @@ export default class SliderDots extends Component {
             <span
               onClick={()=>this.props.turn(index-this.props.index)}
               key={index}
-              className={"dot "+(index==this.props.index?"active":"")}
+              className={"dot "+((index==this.props.index||(this.props.index == this.props.images.length&&index==0)?"active":""))}
             ></span>
           ))
         }
